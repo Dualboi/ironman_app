8cc0ee06266d3c7162b8fa42ca0462dc01633098
sonny bell <sonnybell2004@gmail.com>
2026-04-27 11:09:03 +0100

    connecting new user creation to database and adding prisma client to backend

--- ironman_backend/src/routes/auth.routes.ts
+++ ironman_backend/src/routes/auth.routes.ts
@@ -1,16 +1,35 @@
 import express from "express";
 import { authMiddleware, AuthenticatedRequest } from "../middleware/auth.middleware";
+import { prisma } from "../prisma";
 
 const router = express.Router();
 
 router.post("/me", authMiddleware, async (req: AuthenticatedRequest, res) => {
-  const userId = req.userId;
-  const email = (req.user as any)?.email;
+  const userId = req.userId!;
+  const email = (req.user as any)?.email ?? null;
+
+  if (!email) {
+    return res.status(400).json({ error: "User email missing from token" });
+  }
+
+  // ensures user exists in the DB
+  let user = await prisma.user.findUnique({
+    where: { id: userId },
+  });
+
+  if (!user) {
+    user = await prisma.user.create({
+      data: {
+        id: userId,
+        email,
+        name: "",
+      },
+    });
+  }
 
   return res.json({
     message: "User authenticated",
-    userId,
-    email,
+    user,
   });
 });
 
