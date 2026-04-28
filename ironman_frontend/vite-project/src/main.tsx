224dd8167b174ed70a90c64c6e2837b5a74f54cb
sonny bell <sonnybell2004@gmail.com>
2026-04-28 22:27:07 +0100

    more fixes

--- ironman_frontend/vite-project/src/main.tsx
+++ ironman_frontend/vite-project/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import { AuthProvider } from "./features/auth/AuthContext";
 import "./App.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
