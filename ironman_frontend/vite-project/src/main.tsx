dd89b974eeaba10926a3470dcb7c12730d7052be
sonny bell <sonnybell2004@gmail.com>
2026-04-28 22:23:32 +0100

    tailwind fix maybe

--- ironman_frontend/vite-project/src/main.tsx
+++ ironman_frontend/vite-project/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import { AuthProvider } from "./features/auth/AuthContext";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
