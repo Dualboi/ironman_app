d7f0e161ba2c1c985a35804137606f260b1aad4f
sonny bell <sonnybell2004@gmail.com>
2026-04-28 22:32:00 +0100

    changed name of App.css to index.css and added tailwind test div to main.tsx

--- ironman_frontend/vite-project/src/main.tsx
+++ ironman_frontend/vite-project/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import { AuthProvider } from "./features/auth/AuthContext";
-import "./App.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
