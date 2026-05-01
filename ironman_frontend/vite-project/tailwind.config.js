a0b98b750d765ffa39cdb20906a8ec0c7e262a87
dualboi <sonnybell2004@gmail.com>
2026-05-01 18:14:56 +0100

    Added custom font from google fronts. updated the header to use the new font.

---
+++ ironman_frontend/vite-project/tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter Tight", "sans-serif"],
+        display: ["Inter Tight", "sans-serif"],
+      },
+    },
+  },
+};
