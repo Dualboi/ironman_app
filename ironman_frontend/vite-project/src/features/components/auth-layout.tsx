afd1501a122c82a53618ac48c739cb0e96527b77
dualboi <sonnybell2004@gmail.com>
2026-05-01 18:16:06 +0100

    small text changes in hero image

--- ironman_frontend/vite-project/src/features/components/auth-layout.tsx
+++ ironman_frontend/vite-project/src/features/components/auth-layout.tsx
@@ -3,8 +3,8 @@ export const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className=" min-h-screen bg-gradient-to-br  from-black via-zinc-900 to-black flex flex-col items-center">
             {/* Navbar */}
-            <header className=" font-display text-5xl font-semibold tracking-tight w-full p-4 text-center text-zinc-300 border-b border-zinc-800">
-                Ironman Buddy
+            <header className=" font-display text-3xl font-semibold tracking-tight w-full p-4 text-center text-zinc-300 border-b border-zinc-800">
+                IronMan Buddy
             </header>
 
             {/* Center Content */}
