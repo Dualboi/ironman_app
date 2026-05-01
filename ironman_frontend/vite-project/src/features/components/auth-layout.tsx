446a086cee7d8c996b6104b9bdbcb6c88b83a4f2
dualboi <sonnybell2004@gmail.com>
2026-05-01 18:17:30 +0100

    changed text capitalization of header text

--- ironman_frontend/vite-project/src/features/components/auth-layout.tsx
+++ ironman_frontend/vite-project/src/features/components/auth-layout.tsx
@@ -4,7 +4,7 @@ export const AuthLayout = ({ children }: { children: React.ReactNode }) => {
         <div className=" min-h-screen bg-gradient-to-br  from-black via-zinc-900 to-black flex flex-col items-center">
             {/* Navbar */}
             <header className=" font-display text-3xl font-semibold tracking-tight w-full p-4 text-center text-zinc-300 border-b border-zinc-800">
-                IronMan Buddy
+                IRONMAN BUDDY
             </header>
 
             {/* Center Content */}
