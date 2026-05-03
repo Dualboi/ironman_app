086ef04b6d1f7ee10cff30fec87bf99f281651d3
dualboi <sonnybell2004@gmail.com>
2026-05-03 09:24:27 +0100

    adding changes to navbar

--- ironman_frontend/vite-project/src/shared/components/navbar.tsx
+++ ironman_frontend/vite-project/src/shared/components/navbar.tsx
@@ -4,6 +4,13 @@ import React from "react";
 export const Navbar: React.FC = () => {
   return (
     <nav className="bg-zinc-900 border-b border-zinc-800">
+        <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
+            <div className="font-display text-2xl font-semibold tracking-tight text-zinc-300">
+            </div>
+            <div>
+            {/* Future nav items can go here */}
+            </div>
+        </div>
     </nav>
   );
 };
