ceeb7873b34ee757295723fea2b03127064d050f
sonny bell <sonnybell2004@gmail.com>
2026-05-06 17:32:38 +0100

    fixed formatting on nav items

--- ironman_frontend/vite-project/src/shared/components/navbar.tsx
+++ ironman_frontend/vite-project/src/shared/components/navbar.tsx
@@ -5,10 +5,10 @@ import { NavbarMenuButton } from "./navbar-menu-button";
 export const Navbar: React.FC = () => {
     return (
         <nav className="w-screen bg-zinc-900 border-b border-zinc-800">
-            <div className="max-w-7xl mx-auto px-4 py-3 flex items-right justify-between min-h-[48px]">
+            <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between min-h-[48px]">
                 <div className="font-display text-2xl font-semibold tracking-tight text-zinc-300" />
 
-                <div className="flex items-right">
+                <div className="flex items-center justify-end">
                     <NavbarMenuButton />
                 </div>
             </div>
