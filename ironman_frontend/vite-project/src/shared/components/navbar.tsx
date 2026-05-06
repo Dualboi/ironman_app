18f7320f03947acf1b44b77559dd9f4138c06e4e
sonny bell <sonnybell2004@gmail.com>
2026-05-06 17:30:09 +0100

    removed spacing in nav

--- ironman_frontend/vite-project/src/shared/components/navbar.tsx
+++ ironman_frontend/vite-project/src/shared/components/navbar.tsx
@@ -8,7 +8,7 @@ export const Navbar: React.FC = () => {
             <div className="max-w-7xl mx-auto px-4 py-3 flex items-right justify-between min-h-[48px]">
                 <div className="font-display text-2xl font-semibold tracking-tight text-zinc-300" />
 
-                <div className="flex items-right space-x-2">
+                <div className="flex items-right">
                     <NavbarMenuButton />
                 </div>
             </div>
