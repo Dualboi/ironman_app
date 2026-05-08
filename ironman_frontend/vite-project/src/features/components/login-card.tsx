75b30d885c96c7b8b0694045aa65c036b8d2f345
sonny bell <sonnybell2004@gmail.com>
2026-05-08 21:29:16 +0100

    Adding key log for login with enter key

--- ironman_frontend/vite-project/src/features/components/login-card.tsx
+++ ironman_frontend/vite-project/src/features/components/login-card.tsx
@@ -50,6 +50,11 @@ export const LoginCard = ({
                         placeholder="Password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                                onLogin();
+                            }
+                        }}
                     />
                     <button
                         type="button"
