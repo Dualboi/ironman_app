91673688c878215aac6e4930e6a1fb36ef7d6280
dualboi <sonnybell2004@gmail.com>
2026-05-11 16:55:14 +0100

    welcome user fixes

--- ironman_frontend/vite-project/src/features/dashboard/Dashboard.tsx
+++ ironman_frontend/vite-project/src/features/dashboard/Dashboard.tsx
@@ -4,43 +4,34 @@ import { Footer } from "../../shared/components/footer";
 import { Card } from "../../shared/components/reusable-card";
 
 export default function Dashboard() {
-  const { user, session } = useAuth();
+   const { user} = useAuth();
 
-  return (
-    <div className="min-h-screen bg-linear-to-br from-black via-zinc-900 to-black flex flex-col">
-      <header className="font-display text-2xl font-semibold tracking-tight w-full p-4 text-center text-zinc-300 border-b border-zinc-800">
-        IronMan Buddy
-      </header>
+    return (
+        <div className="min-h-screen bg-linear-to-br from-black via-zinc-900 to-black flex flex-col">
+            <header className="font-display text-2xl font-semibold tracking-tight w-full p-4 text-center text-zinc-300 border-b border-zinc-800">
+                IronMan Buddy
+            </header>
 
-      <Navbar />
+            <Navbar />
 
-      <main className="flex-1 w-full px-4 pt-12 md:pt-14">
-        <div className="w-full max-w-7xl mx-auto py-6 space-y-6">
-          <h2 className="text-zinc-300 text-xl font-semibold">Dashboard</h2>
+            <main className="flex-1 w-full px-4 pt-12 md:pt-14">
+                <div className="w-full max-w-7xl mx-auto py-6 space-y-6">
+                    <h2 className="text-zinc-300 text-xl font-semibold">Dashboard</h2>
 
-          <section>
-            <h3 className="text-zinc-300">Backend /me response: </h3>
-            <pre className="text-zinc-300 whitespace-pre-wrap wrap-break-word max-w-full overflow-auto text-sm md:text-base">
-              {JSON.stringify(user, null, 2)}
-            </pre>
-          </section>
+                    <section>
+                        <Card className="text-center">
+                            <p className="text-zinc-300 text-xl font-semibold">Welcome to your dashboard, {user?.name}!</p>
+                        </Card>
+                    </section>
+                    <section>
+                        <Card className="text-center">
+                            <p className="text-zinc-300 text-xl font-semibold">Welcome to your dashboard2 {user?.name}!</p>
+                        </Card>
+                    </section>
+                </div>
+            </main>
 
-          <section>
-            <h3 className="text-zinc-300">JWT preview:</h3>
-            <pre className="text-zinc-300 max-w-full overflow-auto text-sm md:text-base">
-              {session?.access_token?.slice(0, 25)}...
-            </pre>
-          </section>
+            <Footer />
         </div>
-        <Card className="text-center">
-          <p>Welcome to your dashboard, {user?.name}!</p>
-        </Card>
-        <Card className="text-center">
-          <p>Welcome to your dashboard2 {user?.name}!</p>
-        </Card>
-      </main>
-
-      <Footer />
-    </div>
-  );
+    );
 }
