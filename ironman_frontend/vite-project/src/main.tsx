5fb0073c8c9799970cdca29ee96cc3aa49e635af
sonny bell <sonnybell2004@gmail.com>
2026-05-11 23:03:09 +0100

    installed react router to vite

--- ironman_frontend/vite-project/src/main.tsx
+++ ironman_frontend/vite-project/src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import { AuthProvider } from "./features/auth/AuthContext";
 import "./index.css";
-import { BrowserRouter as BrouserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrouserRouter>
+    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrouserRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
