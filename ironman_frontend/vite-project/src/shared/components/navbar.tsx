cfcc2f6ba9c117f2dcf2d5222ff4a01165229faa
dualboi <sonnybell2004@gmail.com>
2026-05-11 14:32:16 +0100

    Change from screen to full on nav to fix centreing issue

--- ironman_frontend/vite-project/src/shared/components/navbar.tsx
+++ ironman_frontend/vite-project/src/shared/components/navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const Navbar: React.FC = () => {
     return (
-        <nav className="w-screen bg-zinc-900 border-b border-zinc-800">
+        <nav className="w-full bg-zinc-900 border-b border-zinc-800">
             <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between min-h-[48px]">
                 <div className="font-display text-2xl font-semibold tracking-tight text-zinc-300" />
 
