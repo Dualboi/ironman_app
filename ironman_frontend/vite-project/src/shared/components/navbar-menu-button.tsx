8fb9153ed48c5cb5a09bdc23bfe5c7e2e1a18714
dualboi <sonnybell2004@gmail.com>
2026-05-11 15:38:15 +0100

    size changes

--- ironman_frontend/vite-project/src/shared/components/navbar-menu-button.tsx
+++ ironman_frontend/vite-project/src/shared/components/navbar-menu-button.tsx
@@ -13,7 +13,7 @@ export const NavbarMenuButton: React.FC<Props> = ({
     return (
         <button
             type="button"
-            className="group inline-flex w-8 h-12 text-cyan-500 items-center justify-center rounded transition cursor-pointer"
+            className="group inline-flex w-12 h-12 text-cyan-500 items-center justify-center rounded transition cursor-pointer"
             aria-pressed={isOpen}
             onClick={toggle}
         >
