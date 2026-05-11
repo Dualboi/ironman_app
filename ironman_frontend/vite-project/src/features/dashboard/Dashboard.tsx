be725a0f1997771a7a80d9b1e82b8fd17757fcc3
dualboi <sonnybell2004@gmail.com>
2026-05-11 16:57:53 +0100

    dash card changes

--- ironman_frontend/vite-project/src/features/dashboard/Dashboard.tsx
+++ ironman_frontend/vite-project/src/features/dashboard/Dashboard.tsx
@@ -4,10 +4,10 @@ import { Footer } from "../../shared/components/footer";
 import { Card } from "../../shared/components/reusable-card";
 
 export default function Dashboard() {
-   const { user} = useAuth();
+    const { user } = useAuth();
 
     return (
-        <div className="min-h-screen bg-linear-to-br from-black via-zinc-900 to-black flex flex-col">
+        <div className="min-h-screen bg-linear-to-br from-black via-zinc-900 to-black flex flex-center">
             <header className="font-display text-2xl font-semibold tracking-tight w-full p-4 text-center text-zinc-300 border-b border-zinc-800">
                 IronMan Buddy
             </header>
@@ -16,16 +16,14 @@ export default function Dashboard() {
 
             <main className="flex-1 w-full px-4 pt-12 md:pt-14">
                 <div className="w-full max-w-7xl mx-auto py-6 space-y-6">
-                    <h2 className="text-zinc-300 text-xl font-semibold">Dashboard</h2>
-
                     <section>
                         <Card className="text-center">
-                            <p className="text-zinc-300 text-xl font-semibold">Welcome to your dashboard, {user?.name}!</p>
+                            <p className="text-zinc-300 text-xl">Welcome to your dashboard, {user?.name}!</p>
                         </Card>
                     </section>
                     <section>
                         <Card className="text-center">
-                            <p className="text-zinc-300 text-xl font-semibold">Welcome to your dashboard2 {user?.name}!</p>
+                            <p className="text-zinc-300 text-xl">Welcome to your dashboard2 {user?.name}!</p>
                         </Card>
                     </section>
                 </div>
