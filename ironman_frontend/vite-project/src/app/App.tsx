b3af46ac81a493ea3f3a7ace62c95a9528179edf
sonny bell <sonnybell2004@gmail.com>
2026-05-12 11:22:40 +0100

    centering items in loading page

--- ironman_frontend/vite-project/src/app/App.tsx
+++ ironman_frontend/vite-project/src/app/App.tsx
@@ -10,7 +10,7 @@ import ManageWorkouts from "../features/settings/ManageWorkouts";
 export default function App() {
   const { session, loading } = useAuth();
 
-  if (loading) return <body className="bg-zinc-900 backdrop-blur-xl"><div>Loading...</div></body>;
+  if (loading) return <body className="bg-zinc-900 backdrop-blur-xl text-white items-center justify-center"><div className="text-center">Loading...</div></body>;
 
   return (
     <Routes>
